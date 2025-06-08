ea030e2146c5ed1f1a9096158bb963c9ea47d6db
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2025-06-08 13:46:58 +0200

    end task 01

--- 01/Task01.js
+++ 01/Task01.js
@@ -18,14 +18,19 @@ const Task01 = () => {
         setCursorPosition(`[${pageX},${pageY}]`);
     }
 
+    const subject = new Subject();
+    subject.subscribe(event => {
+        setEventTime(event);
+        setEventTagName(event);
+        setEventCursorPosition(event);
+    });
+
     return (
         <section>
             <h1>Task 1</h1>
             
             <div onClick={ event => {
-                setEventTime(event);
-                setEventTagName(event);
-                setEventCursorPosition(event);
+                subject.notify(event);
             }}>
                 <p>
                     <strong>Kliknij wybrany element:</strong> <a>link</a>, <button>button</button>, <span>span</span>
